66fa4663d2718c46aeebd18ed36e2dc7e32ef495
mhsohal <mhbuetdu@gmail.com>
2022-02-27 16:14:48 +0600

    service widget layout 2 and 4

--- agelandelement/includes/widgets/services/layout2.php
+++ agelandelement/includes/widgets/services/layout2.php
@@ -1,116 +1,84 @@
-<?php
-
-echo '<!--service provide-->
+<!--service provide-->
         <section class="service_provide_sec_home2">
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 offset-md-2">
                         <div class="service_title title text-center">
-                            <h3>Services wer provided</h3>
-                            <p>Lorem ipsum dolor sit amet,consetetur sadipscing elitr, Lorem ipsum dolor sit amet, At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren sed diam voluptua.
-                            </p>
+                            <h3><?php echo $settings['title'];?></h3>
+                            <p><?php echo $settings['info'];?></p>
                         </div>
                         <!--/.title-->
                     </div>
                     <div class="w-100"></div>
                     <div class="col-md-12">
-                        <ul class="nav nav-tabs" role="tablist">';
-$loop = 0;
-foreach ($categories as $category) {
-    $loop++;
-    if ($loop == 1) {
-        $act = 'active';
-    } else {
-        $act = '';
-    }
-    echo '<li class="nav-item">
-                                <a class="nav-link active" data-toggle="tab" href="#tabs-1" role="tab">
+                        <ul class="nav nav-tabs" role="tablist">
+                         <?php if ($wp_query1->have_posts()) {
+                            $index = 0;
+                            while ($wp_query1->have_posts()) {
+                                $index++;
+                                if ($index == 1){
+                                    $act = 'active';
+                                    $select = 'true';
+                                }else{
+                                    $act = '';
+                                    $select = 'false';
+                                }
+                                $wp_query1->the_post();
+                        ?>
+                            <li class="nav-item">
+                                <a class="nav-link <?php echo esc_attr($act);?>" data-toggle="tab" href="#tabs-<?php echo get_the_ID();?>" role="tab">
                                     <div class="single_two_in">
-                                        <img src="img/home2/icon/service1.png" alt="#">
-                                        <h4>' . $category->name . '</h4>
+                                        <?php
+                                        $meta = ageland_service_meta('service_icon');
+                                        $met = isset($meta['id']) ? $meta['id'] : '';
+                                        echo wp_get_attachment_image($met);
+                                        ?>
+                                        <h4><?php the_title();?></h4>
                                     </div>
                                     <!--/.single_two_in-->
                                 </a>
-                            </li>';
-}
-echo '</ul>
+                            </li>
+                            <?php    }
+                              wp_reset_postdata();
+                          }?>
+            </ul>
                     </div>
 
                     <div class="col-md-12">
                         <div class="tab-content">
-                            <div class="tab-pane active" id="tabs-1" role="tabpanel">
+                            <?php if ($wp_query1->have_posts()) {
+                            $index2 = 0;
+                            while ($wp_query1->have_posts()) {
+                            $index2++;
+                            if ($index2 == 1){
+                                $act2 = 'show active';
+                            }else{
+                                $act2 = '';
+                            }
+                            $wp_query1->the_post();?>
+                            <div class="tab-pane <?php echo esc_attr($act2);?>" id="tabs-<?php echo get_the_ID();?>" role="tabpanel">
                                 <div class="row">
                                     <div class="col-md-5">
                                         <div class="tab-images text-right">
-                                            <img src="img/home2/software-design.jpg" alt="#">
+                                            <?php the_post_thumbnail('full');?>
                                         </div>
                                     </div>
                                     <div class="col-md-7">
                                         <div class="tabs-content-in">
-                                            <h3>Software Design</h3>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eismod tempor incidunt labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.Risus commodo viverra maecenas accumsan.</p>
-                                            <p>Lorem ipsum dolor sit amet, Stet clita kasd gubergren, stet clita kasd gubergren Lorem ipsum dolor sit amet.</p>
-                                            <a href="#" class="btn">Learn More</a>
+                                            <h3><?php the_title();?></h3>
+                                            <p><?php the_excerpt();?></p>
+                                            <a href="<?php the_permalink();?>" class="btn">Learn More</a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <!--/.tab-pane-->
-                            <div class="tab-pane" id="tabs-2" role="tabpanel">
-                                <div class="row">
-                                    <div class="col-md-5">
-                                        <div class="tab-images text-right">
-                                            <img src="img/home2/software-design.jpg" alt="#">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-7">
-                                        <div class="tabs-content-in">
-                                            <h3>Real Data Analytics</h3>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eismod tempor incidunt labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.Risus commodo viverra maecenas accumsan.</p>
-                                            <p>Lorem ipsum dolor sit amet, Stet clita kasd gubergren, stet clita kasd gubergren Lorem ipsum dolor sit amet.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!--/.tab-pane-->
-                            <div class="tab-pane" id="tabs-3" role="tabpanel">
-                                <div class="row">
-                                    <div class="col-md-5">
-                                        <div class="tab-images text-right">
-                                            <img src="img/home2/software-design.jpg" alt="#">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-7">
-                                        <div class="tabs-content-in">
-                                            <h3>E-commerce Website</h3>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eismod tempor incidunt labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.Risus commodo viverra maecenas accumsan.</p>
-                                            <p>Lorem ipsum dolor sit amet, Stet clita kasd gubergren, stet clita kasd gubergren Lorem ipsum dolor sit amet.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!--/.tab-pane-->
-                            <div class="tab-pane" id="tabs-4" role="tabpanel">
-                                <div class="row">
-                                    <div class="col-md-5">
-                                        <div class="tab-images text-right">
-                                            <img src="img/home2/software-design.jpg" alt="#">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-7">
-                                        <div class="tabs-content-in">
-                                            <h3>Real Data Analytics</h3>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eismod tempor incidunt labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.Risus commodo viverra maecenas accumsan.</p>
-                                            <p>Lorem ipsum dolor sit amet, Stet clita kasd gubergren, stet clita kasd gubergren Lorem ipsum dolor sit amet.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!--/.tab-pane-->
+                            <?php    }
+                                wp_reset_postdata();
+                            }?>
                         </div>
                     </div>
                 </div>
             </div>
             <!--/.container-->
         </section>
-        <!--service provide-->';
+        <!--service provide-->
--- agelandelement/includes/widgets/services/layout4.php
+++ agelandelement/includes/widgets/services/layout4.php
@@ -13,9 +13,9 @@
                     <div class="col-md-12">
                         <div class="ag_tab_section_wrapper">
                             <ul class="nav nav-tabs" role="tablist">
-                                <?php if ($wp_query->have_posts()) {
+                                <?php if ($wp_query1->have_posts()) {
                                 $index = 0;
-                                while ($wp_query->have_posts()) {
+                                while ($wp_query1->have_posts()) {
                                 $index++;
                                 if ($index == 1){
                                     $act = 'active';
@@ -24,14 +24,15 @@
                                     $act = '';
                                     $select = 'false';
                                 }
-                                $wp_query->the_post();?>
+                                $wp_query1->the_post();?>
                                 <li class="nav-item">
                                     <a class="nav-link <?php echo esc_attr($act);?>" id="ag_tab_<?php echo get_the_ID();?>" data-toggle="tab" href="#ag_tab_<?php echo get_the_ID();?>_content" role="tab" aria-controls="ag_tab_<?php echo get_the_ID();?>_content" aria-selected="<?php echo esc_attr($select);?>">
                                         <div class="nav_tab_wrapper">
                                             <div class="nav_tab_icon_wrapper">
                                                 <?php
                                                 $meta = ageland_service_meta('service_icon');
-                                                echo wp_get_attachment_image($meta['id']);
+                                                $met = isset($meta['id']) ? $meta['id'] : '';
+                                                echo wp_get_attachment_image($met);
                                                 ?>
                                             </div>
                                             <?php echo get_that_image($settings['mshape1'], 'services_icon_shape');?>
@@ -44,16 +45,16 @@
                                 }?>
                             </ul>
                             <div class="tab-content">
-                                <?php if ($wp_query->have_posts()) {
+                                <?php if ($wp_query1->have_posts()) {
                                 $index2 = 0;
-                                while ($wp_query->have_posts()) {
+                                while ($wp_query1->have_posts()) {
                                 $index2++;
                                 if ($index2 == 1){
                                     $act2 = 'show active';
                                 }else{
                                     $act2 = '';
                                 }
-                                $wp_query->the_post();?>
+                                $wp_query1->the_post();?>
                                 <div class="tab-pane fade <?php echo esc_attr($act2);?>" id="ag_tab_<?php echo get_the_ID();?>_content" role="tabpanel" aria-labelledby="ag_tab_<?php echo get_the_ID();?>">
                                     <div class="row align-items-center">
                                         <div class="col-lg-5">
@@ -64,7 +65,7 @@
                                         <div class="col-lg-7">
                                             <div class="tab_content_wrapper">
                                                 <h4 class="title"><?php the_title();?></h4>
-                                                <p class="desc"><?php the_content();?></p>
+                                                <p class="desc"><?php the_excerpt();?></p>
                                                 <a href="<?php the_permalink();?>" class="ag_btn btn_1">Read More</a>
                                             </div>
                                         </div>
